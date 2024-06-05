97bbe0213a36dab8f629870b4d63256404f88503
youhei-0009 <57719055+youhei-0009@users.noreply.github.com>
2024-06-05 18:50:54 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>test</div>
+    </>
+  );
+}
+
+export default App;
