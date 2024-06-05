a06929f577b0e22c8fc4bb329bb97ea08fea2133
youhei-0009 <yauchi.youhei0009@gmail.com>
2024-06-05 19:00:43 +0900

    test json placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const onClickUsers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {console.log(err)});
+  }
+  const onClickUsers1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users?id=1").then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {console.log(err)});
+  }
 
   return (
     <>
-      <div>test</div>
+      <div className='App'>
+        <button onClick={onClickUsers}>users</button>
+        <button onClick={onClickUsers1}>users Id=1</button>
+      </div>
     </>
   );
 }
